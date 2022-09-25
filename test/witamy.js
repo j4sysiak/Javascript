9f0915680510955ddd698e5e1b0a720f9ad1e898
Jacek Sysiak <j4sysiak@gmail.com>
2022-09-25 09:49:52 +0200

    25-09-2022 09:50

--- test/witamy.js
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env node
-
-console.log("Witamy w Świecie Node.js");
--- test/wydanie-magazynu.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env node
-
-var date = new Date();
-console.log("Aktualne wydanie to " + (date.getMonth() + 1) + '/' + date.getFullYear());
